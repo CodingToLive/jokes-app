4e820e79a63102769ca384d91ec4a4b8beacdfc3
Rafa <rafael.lozano@engineer.com>
2022-06-18 18:07:20 +0200

    Adding controller changin java version, adding banner and index

--- src/main/java/rafa/springframework/jokesapp/controller/JokeController.java
+++ src/main/java/rafa/springframework/jokesapp/controller/JokeController.java
@@ -1,7 +1,23 @@
 package rafa.springframework.jokesapp.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import rafa.springframework.jokesapp.service.JokeService;
 
-@RestController
+@Controller
 public class JokeController {
+
+    private final JokeService service;
+
+    public JokeController(JokeService service) {
+        this.service = service;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", service.getJoke());
+        return "index";
+    }
 }
